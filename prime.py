c86ad5fc3204f8e3f978c3c34fe68518d6529f6e
Lee jongmin <no3796@naver.com>
2025-04-24 05:33:08 +0900

    v0.2 prime

--- prime.py
+++ prime.py
@@ -1,17 +1,18 @@
+def is_prime(number):
+    for i in range(2, number):
+        print(i)
+        if number % (i * i) == 0:
+            return False
+        else:
+            return True
+    if number == 1:
+        return False
+    return number
+
+
 number = int(input("Enter a number: "))
-is_prime = True
 
-for i in range(2, number):
-    print(i)
-    if number % (i*i) == 0:
-        is_prime = False
-        break
-
-
-if number == 1:
-    is_prime = False
-
-if is_prime:
+if is_prime(number):
     print(number,"소수가 맞습니다")
 else:
     print(number,"소수가 아닙니다")
